1c5214fe1c48c01dc2bd58e53c022f3320ab2edb
AgustinEGarcia <agustinezequiel1997@gmail.com>
2021-08-26 18:09:26 -0300

    Importación de modelos
    En esta parte agregamos modelos, y también que se pueda visualizar en los templates de las paginas de login, registro y logout.

--- preguntados/aplicacion/views.py
+++ preguntados/aplicacion/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context={}
+    return render(request, 'index.html', context)
+
+
+def pagina_registro(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form= CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='participante')
+            user.groups.add(group)
+            participante.objects.create(user=user,nombre=user.username,email=user.email)
+            messages.success(request, 'la carga ha sido exitosa ' + username)
+            return redirect('login')
+
+
+
+    context={'form': form}
+    return render(request,'registro.html',context)
+
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request,'el usuario o la contra, son invalidos')
+            
+    context ={}
+
+    return render(request,'login.html',context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
